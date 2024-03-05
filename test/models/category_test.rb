76cc597e16a5a6f019568ea5eb01d2458876a964
Patrick <dongyang0216@gmail.com>
2024-03-06 02:05:28 +0800

    adding a test in model and system, like checking for creating ,edit,update and delete in category or task

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,8 +1,25 @@
 require "test_helper"
 
 class CategoryTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  # test "the truth" do
+  #   assert true
+  # end
+
+  setup do
+    @user = users(:user_one)
   end
-  puts 'hello'
+
+
+  test "should not save Category without name" do
+    @category = @user.categories.new(name: nil)
+    assert_not @category.save
+  end
+
+  test "should not save Category with name longer than 20 characters" do
+    @category = @user.categories.new(name: "a"*51)
+    assert_not @category.save
+  end
+
+
+
 end
--- test/system/categories_test.rb
+++ test/system/categories_test.rb
@@ -1,10 +1,47 @@
 require "application_system_test_case"
 
-class CategoriesTest < ActiveSupport::TestCase
-  test "visiting index" do
+class CategoriesTest < ApplicationSystemTestCase
+  setup do
+    @user = users(:user_one)
+    @category = categories(:category_one)
 
-    visit categories_url
-    assert_selector "h1", text: "Categories"
+    visit new_user_session_path
+
+    fill_in "user[email]", with: @user.email
+    fill_in "user[password]", with: 'password'
+
+    click_on "Log in"
+
+    assert_text "Signed in successfully.",wait: 10
+  end
+
+  test "creating a new category" do
+    click_link 'Add Category'
+
+    fill_in "Name", with: "System Testing"
+    click_on "Create"
+
+    assert_equal categories_path, current_path
+    assert_text "Category was created successfully."
+  end
+
+
+  test "edit and update category name" do
+    visit edit_category_path(@category)
+
+    fill_in "category[name]", with: "Updated Category Name"
+    click_on "Update"
+
+    assert_equal categories_path, current_path
+    assert_text 'Category was updated successfully.',wait: 10
+
+  end
+  test "delete category" do
+    accept_confirm do
+      click_button 'Delete Category',class: "delete-category-#{@category.id}-button"
+    end
+    assert_equal categories_path, current_path
+    assert_text 'Category was deleted successfully.',wait: 10
   end
 
 end
