b4eae909d764b7036c0debea6dc62d08114838d9
Patrick <dongyang0216@gmail.com>
2024-03-09 11:12:58 +0800

    delete unnecessary method in aplication controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,5 @@ class ApplicationController < ActionController::Base
   def render_404
     render file: "#{Rails.root}/public/404.html", layout: false, status: :not_found
   end
-  protected
-  def after_sign_out_path_for(resource_or_scope)
-    # Redirect to the sign in page
-    new_user_session_path
-  end
-
 
 end
